ac887c5e183014a6380b2e1cd3a83617c2d3f539
dr-prabhjot-kaur <prabhjot.kaur@childrens.harvard.edu>
2023-12-16 13:12:48 -0500

    splitted argument of nZslices

--- EstimateT1fromMP2RAGE.m
+++ EstimateT1fromMP2RAGE.m
@@ -1,4 +1,4 @@
-function [T1map] = EstimateT1fromMP2RAGE(filename, jsonfileinv1,jsonfileinv2, TRFLASH, NzSlices,eff,outputfilename)    
+function [T1map] = EstimateT1fromMP2RAGE(filename, jsonfileinv1,jsonfileinv2, TRFLASH, NzSlices1, NzSlices2, eff,outputfilename)    
 % Example Usage:
 % EstimateT1WithJson('/fileserver/fastscratch/prabh/datasets/thresherepilepsy/flywheel20230224/ForEpilepsyMynames/SUBJECTS/4066815/SESSIONS/3T/ACQUISITIONS/t1w/t1w_MP2RAGE_UNI/t1w_MP2RAGE_UNI.nii.gz','/fileserver/fastscratch/prabh/datasets/thresherepilepsy/flywheel20230224/ForEpilepsyMynames/SUBJECTS/4066815/SESSIONS/3T/ACQUISITIONS/t1w/t1w_MP2RAGE_INV1/t1w_MP2RAGE_INV1.json','/fileserver/fastscratch/prabh/datasets/thresherepilepsy/flywheel20230224/ForEpilepsyMynames/SUBJECTS/4066815/SESSIONS/3T/ACQUISITIONS/t1w/t1w_MP2RAGE_INV2/t1w_MP2RAGE_INV2.json',...
 % 0.0064,[30 120],1.0)
@@ -12,7 +12,7 @@ function [T1map] = EstimateT1fromMP2RAGE(filename, jsonfileinv1,jsonfileinv2, TR
 
     MP2RAGE.TRFLASH=TRFLASH;%0.0064; % TR of the GRE readout
     MP2RAGE.TIs=[jsonData.InversionTime jsonData2.InversionTime];% inversion times - time between middle of refocusing pulse and excitatoin of the k-space center encoding
-    MP2RAGE.NZslices= NzSlices; %[ 30 120];% Slices Per Slab * [PartialFourierInSlice-0.5  0.5]
+    MP2RAGE.NZslices= [NzSlices1, NzSlices2]; %[ 30 120];% Slices Per Slab * [PartialFourierInSlice-0.5  0.5]
     MP2RAGE.FlipDegrees=[jsonData.FlipAngle jsonData2.FlipAngle];% Flip
     MP2RAGE.filename=filename;%'MP2RAGE_UNI.nii' % file
 
@@ -27,6 +27,6 @@ function [T1map] = EstimateT1fromMP2RAGE(filename, jsonfileinv1,jsonfileinv2, TR
     disp(MP2RAGE)
     disp(size(MP2RAGEimg.img))
     [T1map, R1map]=T1estimateMP2RAGE(MP2RAGEimg,MP2RAGE,eff);
-
+    T1map.img=1000.*T1map.img;
     save_untouch_nii(T1map,outputfilename);
 end
